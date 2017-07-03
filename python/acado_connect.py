b80b5ed664a7954dcb0d5ed0d19348567ee4045b
Mansard <nmansard@laas.fr>
2017-07-03 11:38:48 +0200

    Change acado_connect from debug exec to lib with class api.

--- python/acado_connect.py
+++ python/acado_connect.py
@@ -2,93 +2,30 @@ from pinocchio.utils import *
 import pinocchio as se3
 from time import sleep
 from numpy.linalg import inv,norm
-import math
-import time
-import heapq
-import random
-import matplotlib.pylab as plt
 from acado_runner import *
-from pendulum import Pendulum
-
-RANDOM_SEED = int((time.time()%10)*1000)
-print "Seed = %d" %  RANDOM_SEED
-np .random.seed     (RANDOM_SEED)
-random.seed         (RANDOM_SEED)
-
-env                 = Pendulum(1,withDisplay=False)       # Continuous pendulum
-env.withSinCos      = False             # State is dim-3: (cosq,sinq,qdot) ...
-NX                  = env.nobs          # ... training converges with q,qdot with 2x more neurones.
-NU                  = env.nu            # Control is dim-1: joint torque
-
-env.vmax            = 100.
-env.Kf              = 0.2
-env.modulo          = False
-
-env.DT              = 0.05
-env.NDT             = 1
-T                   = .5
-NSTEPS              = int(round(T/env.DT))
-
-DT = env.DT
-
-#x1 = rand(2)
-x1 = np.matrix([ .2, .1]).T
-#x2 = rand(2)
-#x2 = np.matrix([ 0., .0]).T 
-x2 = np.matrix([ .2, -.1]).T 
-
-### Compute polynomial trajectory from x1 to x2
-N = 3                   # Polynom degree
-A = zero([4,N+1])
-b = zero(4)
-
-x = x1.copy()
-hx = []
-hu = []
-for i in range(NSTEPS):
-     t = i*DT
-     A[0,:] =  [ t**i for i in range(N+1) ]
-     A[1,1:] = [ i*t**(i-1) for i in range(1,N+1) ]
-     A[2,:] =  [ T**i for i in range(N+1) ]
-     A[3,1:] = [ i*T**(i-1) for i in range(1,N+1) ]
-
-     b[:2] = x
-     b[2:] = x2
-
-     a = [ (i-1)*i*t**(i-2) for i in range(2,N+1) ]*(inv(A)*b)[2:]
-
-     tau = se3.rnea(env.model,env.data,x[:1],x[1:],a)
-     
-     hx.append(x.copy())
-     hu.append(tau)
-
-     x[1] += a*DT
-     x[0] += x[1]*DT
-
-hx.append(x2)
-hu.append(zero(1))
-X = np.hstack(hx).T
-U = np.hstack(hu).T
-
-plt.ion()
-plt.plot(np.arange(NSTEPS+1)*DT,X)
-plt.plot(np.arange(NSTEPS+1)*DT,U)
-
-
 from acado_runner import AcadoRunner
 
 class AcadoConnect(AcadoRunner):
-     def __init__(self,path="/home/nmansard/src/pinocchio/pycado/build/unittest/connect_pendulum"):
+     def __init__(self,path="/home/nmansard/src/pinocchio/pycado/build/unittest/connect_pendulum",
+                  model=None,data=None):
           AcadoRunner.__init__(self,path)
+          self.model = model
+          if self.model is not None: self.data = model.createData()
+          self.options['istate']   ='/tmp/guess.stx'
+          self.options['icontrol'] = '/tmp/guess.ctl'
+          self.options['oparam'] = '/tmp/mpc.prm'
+
      def setTimeInterval(self,T,T0=0.001):
-          acado.options['horizon'] = T
-          acado.options['Tmin'] = T0
-          acado.options['Tmax'] = T*2
+          self.options['horizon'] = T
+          self.options['Tmin'] = T0
+          self.options['Tmax'] = T*2
 
      def buildInitGuess(self,x0,x1):
           T      = self.options['horizon']
           NSTEPS = self.options['steps']
           DT     = T / NSTEPS
+
+          withControl = self.model is not None and self.data is not None
 
           N = 3                   # Polynom degree
           A = zero([4,N+1])       # Matrix of t**i coefficients
@@ -96,7 +33,7 @@ class AcadoConnect(AcadoRunner):
 
           hx = []
           hu = []
-          x = x1.copy()
+          x = x0.copy()
           for i in range(NSTEPS):
                t = i*DT
                A[0,:] =  [ t**i for i in range(N+1) ]
@@ -105,75 +42,87 @@ class AcadoConnect(AcadoRunner):
                A[3,1:] = [ i*T**(i-1) for i in range(1,N+1) ]
 
                b[:2] = x
-               b[2:] = x2
+               b[2:] = x1
 
                a = [ (i-1)*i*t**(i-2) for i in range(2,N+1) ]*(inv(A)*b)[2:]    # Acceleration
-               tau = se3.rnea(env.model,env.data,x[:1],x[1:],a)                 # Torque
+               if withControl:
+                    tau = se3.rnea(self.model,self.data,x[:1],x[1:],a)          # Torque
+                    hu.append(tau)
      
                hx.append(x.copy())
-               hu.append(tau)
 
                x[1] += a*DT                                                     # Integation v
                x[0] += x[1]*DT                                                  # Integration p
 
-          hx.append(x2)            # Append terminal state
+          hx.append(x1)            # Append terminal state
           hu.append(zero(1))       # Append terminal control (meaningless)
           X = np.hstack(hx).T
           U = np.hstack(hu).T
           
-          guessX = np.vstack([ np.arange(NSTEPS+1)/float(NSTEPS),
-                               X.T, zero(NSTEPS+1).T ]).T
-          guessU = np.vstack([ np.arange(NSTEPS+1)/float(NSTEPS),
-                               U.T ]).T
-
+          # while horizon T is optimized, timescale should be rescaled between 0 and 1 
+          # see http://acado.sourceforge.net/doc/html/d4/d29/example_002.html
+          guessX = np.vstack([ np.arange(NSTEPS+1)/float(NSTEPS), X.T, zero(NSTEPS+1).T ]).T
           np.savetxt(self.options['istate'],guessX)
-          np.savetxt(self.options['icontrol'],guessU)
+
+          if withControl:
+               guessU = np.vstack([ np.arange(NSTEPS+1)/float(NSTEPS),   U.T ]).T
+               np.savetxt(self.options['icontrol'],guessU)
 
           return X,U
 
      def run(self,x0,x1,autoInit=True):
           self.options['finalpos'] = ' '.join([ '%.20f'%f for f in x1.flat[:1] ])
           self.options['finalvel'] = ' '.join([ '%.20f'%f for f in x1.flat[1:] ])
-          if autoInit and 'istate' in self.options and 'icontrol' in self.options:
-               self.buildInitGuess(x0,x1)
+          if autoInit:               self.buildInitGuess(x0,x1)
           return AcadoRunner.run(self,x0.flat[:1],x0.flat[1:])
 
      def initrun(self,x0,x1,i=100,autoInit = True):
           self.options['finalpos'] = ' '.join([ '%.20f'%f for f in x1.flat[:1] ])
           self.options['finalvel'] = ' '.join([ '%.20f'%f for f in x1.flat[1:] ])
-          if autoInit and 'istate' in self.options and 'icontrol' in self.options:
-               self.buildInitGuess(x0,x1)
+          if autoInit:               self.buildInitGuess(x0,x1)
           return AcadoRunner.initrun(self,x0.flat[:1],x0.flat[1:])
 
-#acado = AcadoRunner("/home/nmansard/src/pinocchio/pycado/build/unittest/connect_pendulum")
-acado = AcadoConnect()
-# acado.options['horizon'] = T
-# acado.options['Tmin'] = 0.001
-# acado.options['Tmax'] = T*2
-acado.setTimeInterval(T)
-acado.options['steps']    = NSTEPS
-acado.options['shift']    = 0
-acado.options['iter']     = 100
-acado.options['friction'] = 0.2
-#del acado.options['istate']
-# acado.options['finalpos'] = x2[0,0]
-# acado.options['finalvel'] = x2[1,0]
+     def rerun(self): return AcadoRunner.run(self)
 
-# while horizon T is optimized, timescale should be rescaled between 0 and 1 
-# see http://acado.sourceforge.net/doc/html/d4/d29/example_002.html
-# guessX = np.vstack([ np.arange(NSTEPS+1)/float(NSTEPS),
-#                      X.T, zero(NSTEPS+1).T ]).T
-# guessU = np.vstack([ np.arange(NSTEPS+1)/float(NSTEPS),
-#                      U.T ]).T
+     def states(self):
+         '''The problem is Mayer-based, hence the cost is not part of the state file.'''
+         return np.array(f2a(self.stateFile))[:,1:]
 
-# np.savetxt('/tmp/state.txt',guessX)
-# np.savetxt('/tmp/control.txt',guessU)
+     def params(self):
+         return np.array(f2a(self.options['oparam']))[:,1:]
+     
+     def opttime(self):
+          return self.params()[0,0]
+     
 
-acado.options['istate']='/tmp/guess.stx'
-acado.options['icontrol'] = '/tmp/guess.ctl'
-    
-acado.debug()
-acado.options['iter']=100
-#u,cost = acado.run(X[0,:1],X[0,1:])
-u,cost = acado.run(X[0,:].T,X[-1,:].T)
+if __name__ == '__main__':
+
+     from pendulum import Pendulum
+
+     env                 = Pendulum(1,withDisplay=False)       # Continuous pendulum
+     env.withSinCos      = False             # State is dim-3: (cosq,sinq,qdot) ...
+     NX                  = env.nobs          # ... training converges with q,qdot with 2x more neurones.
+     NU                  = env.nu            # Control is dim-1: joint torque
+
+     env.vmax            = 100.
+     env.Kf              = 0.2
+     env.modulo          = False
+
+     env.DT              = 0.05
+     env.NDT             = 1
+     T                   = .5
+     NSTEPS              = int(round(T/env.DT))
+
+     x1 = np.matrix([ .2, .1]).T
+     x2 = np.matrix([ .2, -.1]).T 
+
+     acado = AcadoConnect(model=env.model)
+     acado.setTimeInterval(T)
+     acado.options['steps']    = NSTEPS
+     acado.options['shift']    = 0
+     acado.options['iter']     = 100
+     acado.options['friction'] = 0.2
+     
+     # acado.debug()
+     u,cost = acado.run(x1,x2)
     
