26b68cac336f254482966f1da6ba77b59433961c
Mansard <nmansard@laas.fr>
2017-09-06 15:16:23 +0200

    Bugs resolved.

--- python/cursor_tricks.py
+++ python/cursor_tricks.py
@@ -1,7 +1,10 @@
 from capture_cursor import *
+import numpy as np
+from pinocchio.utils import *
 
 class FromCursor():
      def __init__(self,prm,grid,env = None):
+          cursorCoordinate.connect()
           self.prm  = prm
           self.grid = grid
           self.hdistance = prm.nearestNeighbor.hdistance
@@ -10,11 +13,11 @@ class FromCursor():
      def node(self):
           '''Get the closest node of the PRM to the cursor coordinate.'''
           x = self.cursorToState(cursorCoordinate())
-          return self.prm.nearestNeighbor(x,graph.x)[0]
+          return self.prm.nearestNeighbor(x,self.prm.graph.x)[0]
      def data(self):
           '''Get the closest point of the grid to the cursor coordinate.'''
           x = self.cursorToState(cursorCoordinate())
-          return self.prm.nearestNeighbor(x,[d.x0 for d in data])[0]
+          return self.prm.nearestNeighbor(x,[d.x0 for d in self.grid.data])[0]
      def playdata(self):
           idx = self.data()
           print self.grid.data[idx].x0
