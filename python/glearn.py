689ef37228845f7c3a627f2e2ebb0fd1312d4ee8
Mansard <nmansard@laas.fr>
2017-09-08 11:03:21 +0200

    Learning next state and trajectory.

--- python/glearn.py
+++ python/glearn.py
@@ -37,12 +37,18 @@ nexs = [ d.X[1,:] for d in data  ]; nexs = np.array(np.vstack(nexs))
 vs   = [ d.cost   for d in data  ]; vs   = np.array(np.vstack(vs))
 '''
 
-nex = lambda X: np.vstack([ X[1:,:], X[-1:,:]])
+#nex = lambda X: np.vstack([ X[1:,:], X[-1:,:]])          # Shift once 
+SHIFT = 4
+nex = lambda X: np.vstack([ X[SHIFT:,:] ] +  [ X[-1:,:] ]*SHIFT)  # shit 5 times
 
 xs =   [ d.X        for d in data ]; xs   = np.array(np.vstack(xs))
 us =   [ d.U        for d in data ]; us   = np.array(np.vstack(us))
 nexs = [ nex(d.X)   for d in data ]; nexs = np.array(np.vstack(nexs))
 vs =   [ d.cost-d.T for d in data ]; vs   = np.array(np.vstack(vs))
+
+subtraj = lambda X,i:    np.ravel(np.vstack([X[i:,:]]+[X[-1:,:]]*i))
+trajs = [ subtraj(d.X,i) for d in data for i in range(len(d.X))  ]; trajs = np.array(np.vstack(trajs))
+
 
 # --- NETS ---------------------------------------------------------
 # --- NETS ---------------------------------------------------------
@@ -50,30 +56,39 @@ vs =   [ d.cost-d.T for d in data ]; vs   = np.array(np.vstack(vs))
 from networks import *
 
 policy = PolicyNetwork(NX,NU,umax=[-1,25]).setupOptim('direct')
-value  = PolicyNetwork(NX,1)         .setupOptim('direct')
+pstate = PolicyNetwork(NX,NX             ).setupOptim('direct')
+ptraj  = PolicyNetwork(NX,trajs.shape[1] ).setupOptim('direct')
+value  = PolicyNetwork(NX,1              ).setupOptim('direct')
 
 sess            = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
 # --- TRAINING -----------------------------------------------------
 BATCH_SIZE = 128
-NEPISODES  = 10000
+NEPISODES  = 50000
 
-REFBATCH = random.sample(range(len(us)),BATCH_SIZE*8)
+def train():
+    REFBATCH = random.sample(range(len(us)),BATCH_SIZE*8)
+    hist = []
+    for episode in range(NEPISODES):
+        if not episode % 100: 
+            print 'Episode #',episode
 
-hist = []
-for episode in range(NEPISODES):
-    if not episode % 100: 
-        print 'Episode #',episode
+        batch = random.sample(range(len(us)),BATCH_SIZE)
+        sess.run(policy.optim, feed_dict={ policy.x     : xs[batch,:],
+                                           policy.uref  : us[batch,:] })
+        sess.run(pstate.optim, feed_dict={ pstate.x     : xs[batch,:],
+                                           pstate.uref  : nexs[batch,:] })
+        sess.run(ptraj .optim, feed_dict={ ptraj .x     : xs[batch,:],
+                                           ptraj .uref  : trajs[batch,:] })
 
-    batch = random.sample(range(len(us)),BATCH_SIZE)
+        if not episode % 10:
+            U = sess.run(policy.policy, feed_dict={ policy.x     : xs[REFBATCH,:] })
+            hist.append( norm(U-us[REFBATCH])/len(REFBATCH) )
 
-    sess.run(policy.optim, feed_dict={ policy.x     : xs[batch,:],
-                                       policy.uref  : us[batch,:] })
+    return hist
 
-    if not episode % 10:
-        U = sess.run(policy.policy, feed_dict={ policy.x     : xs[REFBATCH,:] })
-        hist.append( norm(U-us[REFBATCH])/len(REFBATCH) )
+hist = train()
 
 # --- SAVE ---------------------------------------------------------
 #tf.train.Saver().save(sess,dataRootPath+'/policy')
@@ -119,14 +134,30 @@ def trial(x0 = None,dneigh=3e-1):
         #x = np.matrix([ 1.,0,0, 0,0,0 ]).T
 
     hx = zero([NSTEPS,NX])
+    hy = zero([NSTEPS,NX])
     x = x0.copy()
+    y = x0.copy().T
     print x.T
-    for i in range(300):
+    for i in range(NSTEPS):
+        hx[i,:] = x.T
+        hy[i,:] = y
         u = sess.run(policy.policy, feed_dict={ policy.x: x.T } )
         x = env.dynamics(x,np.matrix(u.T))
-        hx[i,:] = x.T
+        y = sess.run(pstate.policy,feed_dict={ pstate.x: y })
 
-    plt.plot(hx[:,0],hx[:,1],'r',linewidth=2)
     for i in [ i for i,d in enumerate(data) if norm(d.x0-x0)<dneigh ]:
         plt.plot(data[i].X[:,0],data[i].X[:,1],'k')
+    plt.plot(hx[:,0],hx[:,1],'r+-',linewidth=2)
+    plt.plot(hy[:,0],hy[:,1],'b+-',linewidth=2)
+
+    X = sess.run(ptraj.policy,feed_dict={ ptraj.x:x0.T })
+    plt.plot(X[0,::6],X[0,1::6],'g',linewidth=5)
+
+# x0 = zero(6)
+# x  = x0.T
+# hx = []
+# for i in range(50): 
+#     hx.append(x)
+#     x = sess.run(pstate.policy,feed_dict={ pstate.x: x })
+
 
